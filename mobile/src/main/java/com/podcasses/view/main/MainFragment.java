b4c0e032a3572aa2d5da7ccaa1522a456cc8e22a
AleksandarKovachev <akovachev@tu-sofia.bg>
2019-01-13 12:05:42 +0200

    Initialize the project

---
+++ mobile/src/main/java/com/podcasses/view/main/MainFragment.java
@@ -0,0 +1,37 @@
+package com.podcasses.view.main;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.podcasses.R;
+import com.podcasses.viewmodel.MainViewModel;
+
+public class MainFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ mobile/src/main/java/com/podcasses/viewmodel/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.podcasses.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
